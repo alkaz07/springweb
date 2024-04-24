594bc3fc54c03e33a2ba81c712dbc6ac9b308fea
alkaz07 <alkaz07@yandex.ru>
2024-04-24 13:17:52 +0300

    + комментарии

--- src/main/java/alkaz/springweb/MyController1.java
+++ src/main/java/alkaz/springweb/MyController1.java
@@ -8,37 +8,41 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.io.File;
 import java.util.Scanner;
 
+//класс, который знает, как реагировать на разные http-запросы
+//какой запрос каким методом обрабатывать - это mapping
+
 @Controller
 public class MyController1 {
-    @GetMapping("/hello")
+    @GetMapping("/hello")   //реагировать на http запрос localhost:8118/hello  или <IP-адрес>:<Port>/hello
     public String greeting()
     {
-        return "hello";
+        return "hello";  //использовать шаблон hello.html из папки ресурсов
     }
 
-    @GetMapping("/sayname")
+    @GetMapping("/sayname") //реагировать на запрос /syname?name=Вася  или /syname
     public String say(@RequestParam(name="name", required=false)String name, Model model)
     {
-        System.out.println("получен параметр "+name);
-        model.asMap().put("name", name);
+        System.out.println("получен параметр "+name);   //чисто для отладки - вывод  в консоль сервера
+        model.asMap().put("name", name);                //помещаем имя в словарь для использования шаблонизатором Thymeleaf
         int age = (int) (Math.random()*99)+1;
-        model.asMap().put("age", age);
-        return "sayname";
+        model.asMap().put("age", age);                  //помещаем число в словарь
+        return "sayname";                       //использовать шаблон sayname.html из папки ресурсов
     }
-    @GetMapping("/readfile")
+
+    @GetMapping("/readfile")     //реагировать на запрос /readfile?fname=c.txt
     public String readfile(@RequestParam(name="fname") String fname, Model model){
         model.asMap().put("fname", fname);
         try {
-            fname = "src//main//resources//files//"+fname;
+            fname = "src//main//resources//files//"+fname;  //корректируем имя файла с учетом заранее известного каталога
             Scanner scanner = new Scanner(new File(fname));
             String s = scanner.nextLine();
             scanner.close();
-            model.addAttribute("first_line", s);
+            model.addAttribute("first_line", s);        //помещаем прочитанную строчку в словарь
         }
-        catch (Exception e){
-            model.addAttribute("error", e.getMessage());
+        catch (Exception e){                            //если не удалось окрыть файл
+            model.addAttribute("error", e.getMessage());    //помещаем сообщение об ошибке в словарь
         }
-        return "readfile";
+        return "readfile";                     //использовать шаблон readfile.html
     }
 
 }
--- src/main/java/alkaz/springweb/SpringwebApplication.java
+++ src/main/java/alkaz/springweb/SpringwebApplication.java
@@ -3,11 +3,14 @@ package alkaz.springweb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Класс-запускалка
+
 @SpringBootApplication
 public class SpringwebApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringwebApplication.class, args);
+		//запускается встроенный Apache Tomcat и начинает слушать порт, указанный в файле application.properties
 	}
 
 }
